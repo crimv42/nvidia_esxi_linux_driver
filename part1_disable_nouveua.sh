dcee1d808336b573cfd656675ff748c4cf3706e7
Daniel Nelems <daniel.nelems@gmail.com>
2023-10-21 05:41:46 -0600

    finish for now

--- part1_disable_nouveua.sh
+++ part1_disable_nouveua.sh
@@ -9,30 +9,45 @@
 # kernel.                                                     #
 ###############################################################
 
+# Check if the script is run as sudo (EUID 0)
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root using sudo."
+  exit 1
+fi
+
 # Blacklist Nouveau
 blacklist_nouveau() {
-touch /etc/modprobe.d/blacklist-nvidia-nouveau.conf
-cat >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf << EOF
+  touch /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+  cat >>/etc/modprobe.d/blacklist-nvidia-nouveau.conf <<EOF
 blacklist nouveau
 options nouveau modeset=0
 EOF
 }
 
 # Enabled nvdia nvreg_openrmenableupsupportedgpus
-touch /etc/modprobe.d/nvidia.conf
-cat >> /etc/modprobe.d/nvidia.conf << EOF
+enableNVIDIA() {
+  touch /etc/modprobe.d/nvidia.conf
+  cat >>/etc/modprobe.d/nvidia.conf <<EOF
 options nvidia NVreg_OpenRmEnableUnsupportedGpus=1
 EOF
+}
+
+rebootNow() {
+  read -pr "Kernel update successful. Do you want to reboot now? (y/n): " reboot_choice
+  if [ "${reboot_choice}" = "y" ]; then
+    echo "Rebooting the machine in 5 seconds"
+    sleep 5
+    sudo reboot now
+  elif [ "${reboot_choice}" = "n" ]; then
+    echo "Not rebooting the machine."
+  else
+    echo "Invalid choice. Please enter 'y' for yes or 'n' for no."
+  fi
+}
+
+
+blacklist_nouveau
+enableNVIDIA
 sudo update-initramfs -u
 
-read -p "Kernel update successful. Do you want to reboot now? (y/n): " reboot_choice
-
-if [ "$choice" = "y" ]; then
-  echo "Rebooting the machine in 5 seconds"
-  sleep 5
-  sudo reboot now
-elif [ "$choice" = "n" ]; then
-  echo "Not rebooting the machine."
-else
-  echo "Invalid choice. Please enter 'y' for yes or 'n' for no."
-fi
+rebootNow
--- part3_NVENC_patch.sh
+++ part3_NVENC_patch.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+########################################################
+# This step is optional.                               #
+# This patch removes restriction on maximum            #
+# number of simultaneous NVENC video encoding sessions #    
+# imposed by Nvidia to consumer-grade GPUs.            #
+# https://github.com/keylase/nvidia-patch              #
+########################################################
+
 # Patch Driver
 wget -O /tmp/patch.sh https://raw.githubusercontent.com/keylase/nvidia-patch/master/patch.sh
 chmod +x /tmp/patch.sh
